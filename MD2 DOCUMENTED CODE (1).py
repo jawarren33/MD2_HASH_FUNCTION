527f61145cbdad4c08e63392bae05a4c5dbfcc2c
Jamie Warren <73670838+jawarren33@users.noreply.github.com>
2023-05-30 14:57:50 -0400

    Update MD2 DOCUMENTED CODE.py

--- MD2 DOCUMENTED CODE (1).py
+++ MD2 DOCUMENTED CODE (1).py
@@ -5,17 +5,20 @@
 
 # ******KERNEL MUST BE RESTARTED PRIOR TO PROCESSING A NEW MESSAGE******
 
-# In[54]:
-
 
 #Static unsigned character list
 #These characters are digits of the number pi
-S = [41, 46, 67, 201, 162, 216, 124, 1, 61, 54, 84, 161, 236, 240, 6, 19,98, 167, 5, 243, 192, 199, 115, 140, 152, 147, 43, 217, 188, 76, 130, 202,30, 155, 87, 60, 253, 212, 224, 22, 103, 66, 111, 24, 138, 23, 229, 18,190, 78, 196, 214, 218, 158, 222, 73, 160, 251, 245, 142, 187, 47, 238, 122,169, 104, 121, 145, 21, 178, 7, 63, 148, 194, 16, 137, 11, 34, 95, 33,128, 127, 93, 154, 90, 144, 50, 39, 53, 62, 204, 231, 191, 247, 151, 3,255, 25, 48, 179, 72, 165, 181, 209, 215, 94, 146, 42, 172, 86, 170, 198,79, 184, 56, 210, 150, 164, 125, 182, 118, 252, 107, 226, 156, 116, 4, 241,69, 157, 112, 89, 100, 113, 135, 32, 134, 91, 207, 101, 230, 45, 168, 2,27, 96, 37, 173, 174, 176, 185, 246, 28, 70, 97, 105, 52, 64, 126, 15,85, 71, 163, 35, 221, 81, 175, 58, 195, 92, 249, 206, 186, 197, 234, 38,44, 83, 13, 110, 133, 40, 132, 9, 211, 223, 205, 244, 65, 129, 77, 82,106, 220, 55, 200, 108, 193, 171, 250, 36, 225, 123, 8, 12, 189, 177, 74,120, 136, 149, 139, 227, 99, 232, 109, 233, 203, 213, 254, 59, 0, 29, 57,242, 239, 183, 14, 102, 88, 208, 228, 166, 119, 114, 248, 235, 117, 75, 10,49, 68, 80, 180, 143, 237, 31, 26, 219, 153, 141, 51, 159, 17, 131, 20]
-
-
-#Apply the built in python input function
-#The characters that we want encrypted and passed through the MD2 are entered here
-#Input abc into the input function after running
+S = [41, 46, 67, 201, 162, 216, 124, 1, 61, 54, 84, 161, 236, 240, 6, 19,98, 167, 5, 243, 192, 199, 115, 140, 
+     152, 147, 43, 217, 188, 76, 130, 202,30, 155, 87, 60, 253, 212, 224, 22, 103, 66, 111, 24, 138, 23, 229, 
+     18,190, 78, 196, 214, 218, 158, 222, 73, 160, 251, 245, 142, 187, 47, 238, 122,169, 104, 121, 145, 21, 178,
+     7, 63, 148, 194, 16, 137, 11, 34, 95, 33,128, 127, 93, 154, 90, 144, 50, 39, 53, 62, 204, 231, 191, 247, 
+     151, 3,255, 25, 48, 179, 72, 165, 181, 209, 215, 94, 146, 42, 172, 86, 170, 198,79, 184, 56, 210, 150, 164,
+     125, 182, 118, 252, 107, 226, 156, 116, 4, 241,69, 157, 112, 89, 100, 113, 135, 32, 134, 91, 207, 101, 230, 
+     45, 168, 2,27, 96, 37, 173, 174, 176, 185, 246, 28, 70, 97, 105, 52, 64, 126, 15,85, 71, 163, 35, 221, 81, 
+     175, 58, 195, 92, 249, 206, 186, 197, 234, 38,44, 83, 13, 110, 133, 40, 132, 9, 211, 223, 205, 244, 65, 
+     129, 77, 82,106, 220, 55, 200, 108, 193, 171, 250, 36, 225, 123, 8, 12, 189, 177, 74,120, 136, 149, 139, 
+     227, 99, 232, 109, 233, 203, 213, 254, 59, 0, 29, 57,242, 239, 183, 14, 102, 88, 208, 228, 166, 119, 114, 
+     248, 235, 117, 75, 10,49, 68, 80, 180, 143, 237, 31, 26, 219, 153, 141, 51, 159, 17, 131, 20]
 
 inputs = input()
 
@@ -23,16 +26,10 @@ inputs = input()
 #Convert input(string) into a bytearray in utf-8 formatting
 M = bytearray(inputs, 'utf-8')
 
-
-# In[55]:
-
-
 print(M)
 
 
 # ### PADDING PHASE
-
-# In[71]:
 
 
 #Since we are using a 16 byte checksum, assign value of 16 to variable x
@@ -56,23 +53,10 @@ def pad_append(padded):
     return byte_arr
 
 M = M + pad_append(padded)
-
-
-# In[57]:
-
-
 print(M)
 
 
-# 
-# 
-# 
-
 # ### CHECKSUM PHASE
-# 
-
-# In[58]:
-
 
 #clear the checksum so that the checksum = 0
 
@@ -87,9 +71,6 @@ print(clear_checksum)
 
 
 # ### APPENDSUM PHASE
-
-# In[59]:
-
 
 #append 16-byte non-linear checksum
 #if the 16-byte non-linear checksum function is not added to the end of the hash, the hash function is 
@@ -117,17 +98,10 @@ for i in range(len(M) // x):
 #Append the checksum to the original message bytearray
 M = M + C
 
-
-# In[60]:
-
-
 print(M)
 
 
 # ### INITIALIZE MD BUFFER
-
-# In[61]:
-
 
 #set buffer_X = 48 as we are using a 48-byte buffer to compute the message digest
 #by using 48-byte buffer, we are creating 3 blocks of 16-byte length
@@ -146,9 +120,6 @@ buffer = buffer(buffer_X)
 
 
 # ### PROCESS MESSAGE IN 16-BYTE BLOCKS
-
-# In[62]:
-
 
 #process each 16-byte block for the buffer of 48
 
@@ -169,17 +140,10 @@ for i in range(len(M) // x):
             t = buffer[k]
         t = (t + j) % len(S)
 
-
-# In[73]:
-
-
 print(buffer)
 
 
 # ### OUTPUT
-
-# In[74]:
-
 
 #the function outputs a 32-byte output
 #must use zfill(2) as the hex() function defaults by not providing the leading 0
